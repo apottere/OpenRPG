ccda8ba7075662bf83df4eecc1b2bb3cf7f86859
Open RPG <openrpg@modelofnothing.no-ip.org>
2012-10-20 17:14:44 -0400

    Reorganized directories.

--- hidden/auth/logged_in.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Refresh: 3; url=/");
-?>
-<br /><br /><br />
-<div align="center">
-<h1>Invalid option!</h1>
-<p><a href="/">Return</a> to OpenRPG home, or be redirected automatically in 3 seconds...</p>
-</div>
--- hidden/auth/quick_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	unset($_SESSION['logged_in']);
-	session_unset();
-	session_write_close();
-	header("Location: login_manager.php?a=login");
-	exit;
-?>
